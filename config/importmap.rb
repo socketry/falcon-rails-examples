61cce7efd925081a764b2124c7af89b4a4faaf8f
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2026-03-23 12:16:30 +1300

    Update dependencies.

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,5 +8,5 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 
-pin "morphdom" # @2.7.7
-pin "@socketry/live", to: "@socketry--live.js" # @0.16.1
+pin "morphdom" # @2.7.8
+pin "@socketry/live", to: "@socketry--live.js" # @0.16.2
